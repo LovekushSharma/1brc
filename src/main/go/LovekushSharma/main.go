6f43bb3c63621a3fa525b4112024d7f44f9e969b
Lovekush Sharma <LovekushS@talentica-all.com>
2024-05-24 21:41:02 +0530

    parallel optimisation for 12 core computer

--- src/main/go/LovekushSharma/main.go
+++ src/main/go/LovekushSharma/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	optimisedconcurrent "1brc/LovekushSharma/optimisedConcurrent"
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 	strt := time.Now()
 	// serial.Serial("C:\\Users\\lovekushs\\Desktop\\1brc\\data\\measurements_1mil.txt")
 	// fmt.Printf("\nTime taken for 1 million rows:%v", time.Since(strt))
@@ -16,6 +18,6 @@ func main() {
 	// strt = time.Now()
 	// serial.Serial("C:\\Users\\lovekushs\\Desktop\\1brc\\data\\measurements_1bil.txt")
 	// fmt.Printf("\nTime taken for 1 billion rows:%v", time.Since(strt))
-	optimisedconcurrent.Optimisedconcurrent("C:\\Users\\lovekushs\\Desktop\\1brc\\data\\measurements_1bil.txt")
+	optimisedconcurrent.Optimisedconcurrent("C:\\Users\\lovekushs\\Desktop\\Program\\1brc\\data\\measurements_1bil.txt")
 	fmt.Println("\n", time.Since(strt))
 }
